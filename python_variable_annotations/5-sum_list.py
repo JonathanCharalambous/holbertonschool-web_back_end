25075d73117c78a5c95bc6401819b0ce0a43631a
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-07-29 17:01:00 +1000

    Update 5-sum_list.py

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+from typing import List
 
 """Return the sum of a list of floating-point numbers."""
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     input_list: A list of floating-point numbers.
     Return: The sum of all elements in input_list.

404d7525a92fe30c2469dc22f02ca7131b621778
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-07-31 00:23:25 +1000

    Update 0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Asynchronous generator that yields random floating-point numbers."""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yields 10 random floating-point numbers between 0 and 10."""
     for _ in range(10):
         await asyncio.sleep(1)

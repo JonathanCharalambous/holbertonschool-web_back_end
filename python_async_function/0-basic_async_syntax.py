6729beb93fd47ea19dd2935d04d6afb75bb77dc9
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-07-30 12:25:11 +1000

    task0

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Return a random floating-point number after a delay."""
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    max_delay: The maximum delay in seconds.
+    Return: A random floating-point number after a delay.
+    """
+    time = random.uniform(0, max_delay)
+    await asyncio.sleep(time)
+    return time

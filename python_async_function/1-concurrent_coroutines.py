024df41ea7089a11d505a7f4c54260281db8e6d2
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-07-30 13:06:10 +1000

    task1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Return a list of floating-point numbers in ascending order after waiting."""
+import asyncio
+from basic_async_syntax import wait_random
+
+async def wait_n(n: int, max_delay: int = 10) -> list[float]:
+    """
+    n: The number of times to wait.
+    max_delay: The maximum delay to wait for each call.
+    """
+    times = []
+    ordered_times = []
+    for _ in range(n):
+        time = wait_random(max_delay)
+        times.append(time)
+       
+    for procedure in asyncio.as_completed(times):
+        ordered_time = await procedure
+        ordered_times.append(ordered_time)
+
+    return ordered_times

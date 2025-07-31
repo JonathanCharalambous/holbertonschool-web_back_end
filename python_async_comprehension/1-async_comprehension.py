1ff04e02293b06de25daaacfc589ac50e6e6e4d5
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-07-31 11:34:33 +1000

    Update 1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,9 +3,7 @@
 from async_generator import async_generator
 from typing import List
 
+
 async def async_comprehension() -> List[float]:
     """Collects 10 random floating-point numbers from async_generator."""
-    result = []
-    async for num in async_generator():
-        result.append(num)
-    return result
+    return [num async for num in async_generator()]
